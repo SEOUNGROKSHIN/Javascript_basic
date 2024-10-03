ab40b39eec315aedcecc530fe3ee152913cf4660
SHINSEOUNGROK <tnsfhr03@gmail.com>
2024-10-03 17:42:59 +0900

    Jquery event-1

---
+++ 자바스크립트 작업/5장(함수와 이벤트)/js/event.js
@@ -0,0 +1,10 @@
+// 상세설명 함수
+function showDetail() {
+    document.querySelector("#desc").style.display = "block";
+    document.querySelector("#open").style.display = "none";
+}
+// 상세설명 닫기 함수
+function hideDetail() {
+    document.querySelector("#desc").style.display = "none";
+    document.querySelector("#open").style.display = "block";
+}
