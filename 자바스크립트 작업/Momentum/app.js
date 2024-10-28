65044c394cbc4547d9cafa15be0dd9c5bd07325e
SHINSEOUNGROK <tnsfhr03@gmail.com>
2024-10-28 23:48:15 +0900

    LoginEvent - 2

--- 자바스크립트 작업/Momentum/app.js
+++ 자바스크립트 작업/Momentum/app.js
@@ -6,6 +6,13 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 loginButton.addEventListener("click", function () {
-    console.dir(loginInput);
-    console.log("clicked");
-})
+   const userName = loginInput.value;
+   //  JS Validation
+   // if (userName === "") {
+   //     alert("Please write your name");
+   // } else if (userName.length > 15) {
+   //     alert("Your name is too long");
+   // }
+
+    console.log(userName);
+});
