fb704b6edc3c56d6bc45e7df70a076dc502e0071
SHINSEOUNGROK <tnsfhr03@gmail.com>
2024-10-30 21:56:23 +0900

    LoginEvent - 3 , Template Literal backtic ` `

--- 자바스크립트 작업/Momentum/app.js
+++ 자바스크립트 작업/Momentum/app.js
@@ -1,14 +1,17 @@
-// const loginForm = document.querySelector("#login-form")
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 // JS는 이벤트에 대한 정보를 (event)인자에 담음 그래서 event를 호출하게되면 submit에 대한 정보를 가져올 수 있음.
 function onLoginBtnClick(event) {
    event.preventDefault();
-   console.log(loginInput.value);
+   const username = loginInput.value;
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   console.log(username);
+   greeting.innerText = `Hello ${username}`;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginBtnClick);
