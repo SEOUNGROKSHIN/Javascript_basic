ead086190328c77ece8ad92b6c3e5c56f700256a
SHINSEOUNGROK <tnsfhr03@gmail.com>
2024-11-05 22:51:33 +0900

    setTimeout, padStart

--- 자바스크립트 작업/Momentum/js/clock.js
+++ 자바스크립트 작업/Momentum/js/clock.js
@@ -1,10 +1,23 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    const hour = String(date.getHours()).padStart(2, "0");
+    const minute = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerHTML = (`${hour}:${minute}:${seconds}`);
 }
 
+
+// setInterval(function, 5000)
 // 2개의 인자를 받음 ,
 // 1. function
 // 2. 초 설정
-setInterval(sayHello, 5000)
+
+// setTimeout(function,5000)
+// 2개의 인자를 받음
+// 1. function
+// 2. 기다리는 시간 뒤에 동작
+
+getClock();
+setInterval(getClock, 1000);
