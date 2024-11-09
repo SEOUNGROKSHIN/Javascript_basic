e5ee5c9d1cfd5f704d8210a5c78fb497f6d35975
SHINSEOUNGROK <tnsfhr03@gmail.com>
2024-11-09 13:02:35 +0900

    TodoList filter Function

--- 자바스크립트 작업/Momentum/js/greetings.js
+++ 자바스크립트 작업/Momentum/js/greetings.js
@@ -1,5 +1,5 @@
-// const loginInput = document.querySelector("#login-form input");
-// const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
 
 // String형식의 반복되는 데이터는 상수처리를해서 대문자로 변수명을 지정하는 것이 좋음 JS에서는 에러를 잡아주지 않기 때문에
@@ -8,13 +8,13 @@ const USERNAME_KEY ="username";
 
 // JS는 이벤트에 대한 정보를 (event)인자에 담음 그래서 event를 호출하게되면 submit에 대한 정보를 가져올 수 있음.
 function onLoginBtnClick(event) {
-   event.preventDefault();
-   // loginForm.classList.add(HIDDEN_CLASSNAME);
-   // const username = loginInput.value;
+   // event.preventDefault();
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   const username = loginInput.value;
  
    // localstorage = 가상의 DB (뷰의 역할과 비슷함 & 휘발성 메모리)
    localStorage.setItem(USERNAME_KEY, username);
-   paintGreetings(username);
+   paintGreetings(USERNAME_KEY);
 }
 
 function paintGreetings(username) {
@@ -26,9 +26,10 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
    // show the form
-   // loginForm.classList.remove(HIDDEN_CLASSNAME);
-   // loginForm.addEventListener("submit", onLoginBtnClick);
+   loginForm.classList.remove(HIDDEN_CLASSNAME);
+   loginForm.addEventListener("submit", onLoginBtnClick);
 } else {
    // show the greetings
    paintGreetings(savedUsername);
+   loginForm.classList.add(HIDDEN_CLASSNAME);
 }
--- 자바스크립트 작업/Momentum/js/todo.js
+++ 자바스크립트 작업/Momentum/js/todo.js
@@ -1,48 +1,54 @@
 const toDoForm = document.querySelector("#todo-form");
 const toDoList = document.querySelector("#todo-list");
 const toDoInput = document.querySelector("#todo-form input");
-const toDos = [];
+let toDos = [];
 const TODOS_KEY = "todos"
+
 function saveTodos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteTodo(event) {
-    // console.log(event.target);
     const li = event.target.parentNode;
     li.remove();
+    toDos = todos.filter(todo => todo.id !== parseInt(li.id));
+    saveTodos();
 }
 
 
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveTodos();
 }
 
 function paintTodo(newTodo) {
     const li = document.createElement("li");
-    // 속성 부여
-    li.id = "item";
     const span = document.createElement("span");
     const button = document.createElement("button");
-    span.innerHTML = newTodo;
+    // 속성 부여
+    li.id = newTodo.id;
+
+    span.innerHTML = newTodo.text;
     button.innerHTML = "❌"
+
     // li 태그안에 span 태그를 넣어줌
     li.appendChild(span);
     li.appendChild(button);
+
     // id : todo-List 요소에 <li><span><span></li> 저장
     toDoList.appendChild(li);
     // button에 이벤트 추가
     button.addEventListener("click", deleteTodo);
 }
 
-function sayHello(item,idx) {
-    console.log("hello item" + idx + ":" + item);
-}
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
 
@@ -50,7 +56,29 @@ const savedTodos = localStorage.getItem(TODOS_KEY);
 
 if (saveTodos !== null) {
     const parsedTodos = JSON.parse(savedTodos);
-    // parsedTodos.forEach(sayHello);
+
     // arrow function
-    parsedTodos.forEach((item, idx) => console.log(`hello Item ${idx} : ${item}`));
+    // parsedTodos.forEach((item, idx) => console.log(`hello Item ${idx} : ${item}`));
+
+    parsedTodos.forEach(paintTodo);
+    todos = parsedTodos;    // localStorage에 있는 데이터를 가져옴
+}
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9].filter() =>
+// 1. filter Function 필요 //
+// 2. 무조건 true를 반환해야 함.
+// 3이상의 숫자는 제거
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// 일반적인 방법
+function sexyFilter(num) {return num <= 3}  // 남겨둘 숫자들의 조건
+let numbers = arr.filter(sexyFilter);
+// 화살표 함수
+const filterArr = arr.filter(item => item <=3 );
+
+console.log(filterArr);
+console.log(numbers);
+
+function sayHello(item,idx) {
+    console.log("hello item" + idx + ":" + item);
+    // parsedTodos.forEach(sayHello);
 }
