803e7d84cc628be5c9154498d2b72ef7611f0155
SHINSEOUNGROK <tnsfhr03@gmail.com>
2024-11-21 23:49:38 +0900

    241121 fastcampus Starbucks - promotion horizontally Slider

--- 패스트캠퍼스/STARBUCKS/js/main.js
+++ 패스트캠퍼스/STARBUCKS/js/main.js
@@ -57,3 +57,20 @@ new Swiper(".notice-line .swiper", {
     autoplay: true,
     loop: true
 })
+new Swiper(".promotion .swiper", {
+    slidesPerView:  3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이에 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보여짐
+    loop: true,
+    autoplay: {
+        delay: 3000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation : {
+        prevEl : '.promotion .swiper-prev',
+        nextEl : '.promotion .swiper-next',
+    }
+});
